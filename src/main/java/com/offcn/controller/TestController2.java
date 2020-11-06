2deb1215539ca055313a27f8a2293ed0f12070ca
sunspot-java <sunspotzxy@163.com>
2020-11-06 13:20:42 +0800

    修改测试类02 分支1

--- src/main/java/com/offcn/controller/TestController2.java
+++ src/main/java/com/offcn/controller/TestController2.java
@@ -3,5 +3,6 @@ package com.offcn.controller;
 public class TestController2 {
     public static void main(String[] args) {
         System.out.println("test2");
+        System.out.println("fenzhi1");
     }
 }
